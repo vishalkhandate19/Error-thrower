6c8c48f8d84ffd0f5bd69133e37fa9ba4553f168
sandeshmurdia <sandeshmurdia123@gmail.com>
2023-01-22 12:55:08 +0530

    first

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+app.use(cors({
+    origin: "*"
+}))
+app.get('/api/error/:status',(req,res)=>{
+    const status= req.params.status
+        res.status(status).send(`Status ${status}`);
+});
+
+const port= process.env.PORT || 4000;
+app.listen(port, () =>console.log(`port ${port}`)  )
