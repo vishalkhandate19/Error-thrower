b13ff58b0121a76b2d1a43f22d8dfb2c471cd514
sandeshmurdia <sandeshmurdia123@gmail.com>
2023-01-23 15:35:37 +0530

    added 500

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.post('/api/error/:status',(req,res)=>{
     const status= req.params.status
         res.status(status).send(`Status ${status}`);
 });
+app.get('/api/error/500/:uuid',(req,res)=>{
+        res.status(500).send(`OK `);
+});
 
 const port= process.env.PORT || 4000;
 app.listen(port, () =>console.log(`port ${port}`)  )
